26e0ea3adad7d8b9410e612fc2f069bd106f1aa8
DiegoFerreira <diegofs2308@gmail.com>
2023-10-06 17:42:04 -0300

    Eu realizei o projeto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MainscreenComponent } from './components/mainscreen/mainscreen.component';
+import { ResultadoComponent } from './components/login/resultado/resultado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainscreenComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'loginefetuado', component: ResultadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { BottombarComponent } from './components/widgets/bottombar/bottombar.component';
+import { TopbarComponent } from './components/widgets/topbar/topbar.component';
+import { MainscreenComponent } from './components/mainscreen/mainscreen.component';
+import { ResultadoComponent } from './components/login/resultado/resultado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BottombarComponent,
+    TopbarComponent,
+    MainscreenComponent,
+    ResultadoComponent,
   ],
   imports: [
     BrowserModule,
